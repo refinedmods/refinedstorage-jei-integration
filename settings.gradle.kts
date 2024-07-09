82b55eace6ce29cf6c28ba2e7e8469daefc24045
raoulvdberge <raoulvdberge@gmail.com>
2024-07-09 23:30:21 +0200

    build: no need dependencyresolutionmgmt

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,3 @@
-dependencyResolutionManagement {
-    repositories {
-        maven {
-            url = uri("https://maven.pkg.github.com/refinedmods/refinedarchitect")
-            credentials {
-                username = "anything"
-                password = "\u0067hp_oGjcDFCn8jeTzIj4Ke9pLoEVtpnZMP4VQgaX"
-            }
-        }
-    }
-    versionCatalogs {
-        create("libs") {
-            from("com.refinedmods.refinedarchitect:refinedarchitect-versioning:0.16.2")
-        }
-    }
-}
-
 pluginManagement {
     repositories {
         mavenCentral()
@@ -37,7 +20,6 @@ pluginManagement {
     }
     plugins {
         id("refinedarchitect.root").version("0.16.2")
-        id("refinedarchitect.base").version("0.16.2")
         id("refinedarchitect.common").version("0.16.2")
         id("refinedarchitect.neoforge").version("0.16.2")
         id("refinedarchitect.fabric").version("0.16.2")
