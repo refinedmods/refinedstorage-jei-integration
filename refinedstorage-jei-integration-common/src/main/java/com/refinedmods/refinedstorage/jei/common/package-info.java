0f9237379bd8cfb0c81480a72b7d1f35b8950e7c
raoulvdberge <raoulvdberge@gmail.com>
2024-07-14 00:57:52 +0200

    fix: add missing annotation

--- refinedstorage-jei-integration-common/src/main/java/com/refinedmods/refinedstorage/jei/common/package-info.java
+++ refinedstorage-jei-integration-common/src/main/java/com/refinedmods/refinedstorage/jei/common/package-info.java
@@ -1,4 +1,7 @@
 @ParametersAreNonnullByDefault
+@FieldsAndMethodsAreNonnullByDefault
 package com.refinedmods.refinedstorage.jei.common;
+
+import com.refinedmods.refinedstorage.api.core.FieldsAndMethodsAreNonnullByDefault;
 
 import javax.annotation.ParametersAreNonnullByDefault;
